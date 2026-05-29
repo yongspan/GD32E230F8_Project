2d6cd140794cd016926a2ebc3e2825482cfebe89
yongspan <135420917+yongspan@users.noreply.github.com>
2026-05-29 18:17:50 +0800

    feat(backlight): complete PWM dimming via UART command
    - Add TPS61165 PWM backlight control
    - Move PWM output to PA6 TIMER2_CH0
    - Add pwm command to cmd_parser
    - Verify brightness control through UART
    - Update PWM driver

--- firmware/app/cmd_parser.c
+++ firmware/app/cmd_parser.c
@@ -1,8 +1,9 @@
 #include "cmd_parser.h"
 #include "bsp_led.h"
+#include "bsp_pwm.h"
 #include <stdio.h>
 #include <string.h>
-
+#include <stdlib.h>
 
 #define CMD_BUFFER_SIZE 32
 
@@ -12,7 +13,6 @@ static uint8_t led_blink_enable = 0;
 
 void cmd_parser_process_char(char ch)
 {
-	  
     if(ch == ';')
     {
         cmd_buffer[cmd_index] = '\0';
@@ -20,65 +20,88 @@ void cmd_parser_process_char(char ch)
         printf("CMD:%s\r\n", cmd_buffer);
 
         if(strcmp(cmd_buffer, "led1 on") == 0)
-{
-    led_blink_enable = 0;
-    bsp_led1_on();
-    printf("LED1 ON\r\n");
-}
-else if(strcmp(cmd_buffer, "led1 off") == 0)
-{
-    led_blink_enable = 0;
-    bsp_led1_off();
-    printf("LED1 OFF\r\n");
-}
-else if(strcmp(cmd_buffer, "led2 on") == 0)
-{
-    led_blink_enable = 0;
-    bsp_led2_on();
-    printf("LED2 ON\r\n");
-}
-else if(strcmp(cmd_buffer, "led2 off") == 0)
-{
-    led_blink_enable = 0;
-    bsp_led2_off();
-    printf("LED2 OFF\r\n");
-}
-else if(strcmp(cmd_buffer, "blink1") == 0)
-{
-    led_blink_enable = 1;
-    printf("LED1 BLINK ENABLE\r\n");
-}
-else if(strcmp(cmd_buffer, "all off") == 0)
-{
-    led_blink_enable = 0;
-    bsp_led1_off();
-    bsp_led2_off();
-    printf("ALL LED OFF\r\n");
-}
-else if(strcmp(cmd_buffer, "status") == 0)
-{
-    printf("System Status:\r\n");
-    printf("Clock: IRC8M\r\n");
-    printf("UART: OK\r\n");
-    printf("Command End: ;\r\n");
-    printf("Blink1: %s\r\n", led_blink_enable ? "ON" : "OFF");
-}
-else if(strcmp(cmd_buffer, "help") == 0)
-{
-    printf("Commands:\r\n");
-    printf("led1 on;\r\n");
-    printf("led1 off;\r\n");
-    printf("led2 on;\r\n");
-    printf("led2 off;\r\n");
-    printf("blink1;\r\n");
-    printf("all off;\r\n");
-    printf("status;\r\n");
-    printf("help;\r\n");
-}
-else
-{
-    printf("Unknown CMD:%s\r\n", cmd_buffer);
-}
+        {
+            led_blink_enable = 0;
+            bsp_led1_on();
+            printf("LED1 ON\r\n");
+        }
+        else if(strcmp(cmd_buffer, "led1 off") == 0)
+        {
+            led_blink_enable = 0;
+            bsp_led1_off();
+            printf("LED1 OFF\r\n");
+        }
+        else if(strcmp(cmd_buffer, "led2 on") == 0)
+        {
+            led_blink_enable = 0;
+            bsp_led2_on();
+            printf("LED2 ON\r\n");
+        }
+        else if(strcmp(cmd_buffer, "led2 off") == 0)
+        {
+            led_blink_enable = 0;
+            bsp_led2_off();
+            printf("LED2 OFF\r\n");
+        }
+        else if(strcmp(cmd_buffer, "blink1") == 0)
+        {
+            led_blink_enable = 1;
+            printf("LED1 BLINK ENABLE\r\n");
+        }
+        else if(strcmp(cmd_buffer, "all off") == 0)
+        {
+            led_blink_enable = 0;
+            bsp_led1_off();
+            bsp_led2_off();
+            printf("ALL LED OFF\r\n");
+        }
+        else if(strncmp(cmd_buffer, "pwm ", 4) == 0)
+        {
+            int duty = atoi(&cmd_buffer[4]);
+
+            if(duty < 0)
+            {
+                duty = 0;
+            }
+
+            if(duty > 100)
+            {
+                duty = 100;
+            }
+
+            bsp_pwm_set_duty((uint8_t)duty);
+            printf("PWM duty:%d%%\r\n", duty);
+        }
+        else if(strcmp(cmd_buffer, "status") == 0)
+        {
+            printf("System Status:\r\n");
+            printf("Clock: IRC8M / SystemCoreClock\r\n");
+            printf("UART: OK\r\n");
+            printf("Command End: ;\r\n");
+            printf("Blink1: %s\r\n", led_blink_enable ? "ON" : "OFF");
+            printf("PWM Duty: %d%%\r\n", bsp_pwm_get_duty());
+        }
+        else if(strcmp(cmd_buffer, "help") == 0)
+        {
+            printf("Commands:\r\n");
+            printf("led1 on;\r\n");
+            printf("led1 off;\r\n");
+            printf("led2 on;\r\n");
+            printf("led2 off;\r\n");
+            printf("blink1;\r\n");
+            printf("all off;\r\n");
+            printf("pwm 0;\r\n");
+            printf("pwm 1;\r\n");
+            printf("pwm 10;\r\n");
+            printf("pwm 50;\r\n");
+            printf("pwm 100;\r\n");
+            printf("status;\r\n");
+            printf("help;\r\n");
+        }
+        else
+        {
+            printf("Unknown CMD:%s\r\n", cmd_buffer);
+        }
 
         cmd_index = 0;
     }
@@ -95,4 +118,3 @@ uint8_t cmd_parser_get_blink_enable(void)
 {
     return led_blink_enable;
 }
-
